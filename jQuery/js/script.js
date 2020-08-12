342511f813761bf0007927e29d43cf667b74a732
jwgainey <jwgainey@hotmail.com>
2020-08-12 18:32:59 -0400

    event methods

--- jQuery/js/script.js
+++ jQuery/js/script.js
@@ -3,9 +3,9 @@
 $(function() {
 
     //alert("Hello?");
-        $('button').click(function() {
-            $('#box').fadeOut(1000);
-        });
+        // $('button').click(function() {
+        //     $('#box').fadeOut(1000);
+        // });
 
         // $('h2,h3,h4').css('border','solid 1px red');
     
@@ -18,6 +18,38 @@ $(function() {
         // $('div > p').css('border','solid 1px red');
         // $(':header').css('border','solid 1px red');
 
-        $(':contains("Joel")').css('border','solid 1px red');
+        //$(':contains("Joel")').css('border','solid 1px red');
+
+        /* -------------------------
+            jQUERY EVENT METHODS
+        ---------------------------*/
+
+        $('#box').click(function() {
+            alert('you just clicked the box!');
+        });
+
+        $("input").blur(function() {
+
+            if( $(this).val() == "") {
+                $(this).css('border', 'solid 1px red');
+                $('#box').text('Forgot to add text?');
+            }
+        });
+
+        $("input").keydown(function() {
+
+            if( $(this).val() !== "") {
+                $(this).css('border', 'solid 1px #777');
+                $('#box').text('Thanks, man.');
+            }
+        });
+
+        $('#box').hover(function() {
+            $(this).text('You hovered in!'); 
+        }, function () {
+            $(this).text('You hovered out!');
+        });
+
+
 });
 
