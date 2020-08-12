3bacfa2b3361ea6ef6ba4cada9bf1dca97f21875
jwgainey <jwgainey@hotmail.com>
2020-08-12 17:49:29 -0400

    selectors

--- jQuery/js/script.js
+++ jQuery/js/script.js
@@ -7,4 +7,17 @@ $(function() {
             $('#box').fadeOut(1000);
         });
 
+        // $('h2,h3,h4').css('border','solid 1px red');
+    
+        // $('div#container').css('border','solid 1px red');
+        // $('p.lead').css('border','solid 1px red');
+
+        // $('li:even').css('border','solid 1px red');
+        // $('div em').css('border','solid 1px red');
+
+        // $('div > p').css('border','solid 1px red');
+        // $(':header').css('border','solid 1px red');
+
+        $(':contains("Joel")').css('border','solid 1px red');
 });
+
